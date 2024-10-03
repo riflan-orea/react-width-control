29e3f5adf06f48c869374431de5f5233aacb1822
Riflan Ahamed <info@oreadigital.com>
2024-10-03 21:16:30 +0530

    fix: react-component min-width support

--- src/MinWidth.tsx
+++ src/MinWidth.tsx
@@ -1,32 +1,52 @@
-import React, { ReactElement, CSSProperties } from "react";
+import React, {
+  ReactElement,
+  CSSProperties,
+  forwardRef,
+  Ref,
+  createElement,
+} from "react";
+
+/**
+ * Props for the MinWidth component
+ * @property {ReactElement} children - The children to apply the min-width
+ * @property {number} size - The size of the min-width
+ */
 
 type MinWidthProps = {
   children: ReactElement;
   size: number;
 };
 
-const MinWidth: React.FC<MinWidthProps> = ({ children, size }) => {
-  const childStyle = (children.props.style as CSSProperties) || {};
-  const combinedStyle: CSSProperties = {
-    ...childStyle,
-    minWidth: `${size}px`,
-  };
+const MinWidth = forwardRef<HTMLDivElement, MinWidthProps>(
+  ({ children, size, ...props }, ref: Ref<HTMLDivElement>) => {
+    const childStyle = (children.props.style as CSSProperties) || {};
+    const minWidthValue = `${size}px`;
 
-  // Check if the child is a React component
-  if (
-    typeof children.type === "function" ||
-    typeof children.type === "object"
-  ) {
-    return React.createElement(children.type, {
+    const combinedStyle: CSSProperties = {
+      ...childStyle,
+      minWidth: minWidthValue,
+    };
+
+    // if children is react component
+    if (
+      typeof children.type === "function" ||
+      typeof children.type === "object"
+    ) {
+      return (
+        <div ref={ref} style={combinedStyle} {...props}>
+          {children}
+        </div>
+      );
+    }
+
+    // Apply min-width for native HTML elements
+    return createElement(children.type, {
       ...children.props,
       style: combinedStyle,
     });
   }
+);
 
-  return React.createElement(children.type, {
-    ...children.props,
-    style: combinedStyle,
-  });
-};
+MinWidth.displayName = "MinWidth";
 
 export default MinWidth;
